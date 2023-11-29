dbcddea54f26876a447a322161d849cfe720b744
yachujoshi-lf <yachujoshi@lftechnology.com>
2023-11-29 13:42:15 +0545

    Refactor: Setup Configurations

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { useRef } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClientRef = useRef(new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientRef.current}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
       <ReactQueryDevtools />
     </QueryClientProvider>
--- apps/client/src/components/NavBar/index.tsx
+++ apps/client/src/components/NavBar/index.tsx
@@ -1,62 +1,50 @@
-"use client";
+'use client';
 
-import {
-  IconUser,
-  IconLogout,
-  IconTrophy,
-  IconSettings,
-  IconSwitchHorizontal,
-} from "@tabler/icons-react";
-import { Box, Flex, Group, NavLink, Title } from "@mantine/core";
-import { useState } from "react";
+import Link from 'next/link';
+import { useState } from 'react';
+import { Flex, Group, NavLink, Title } from '@mantine/core';
+import { IconUser, IconLogout, IconTrophy, IconSettings, IconSwitchHorizontal } from '@tabler/icons-react';
 
 const data = [
-  { link: "/challenges", label: "Challenges", icon: IconTrophy },
-  { link: "/users", label: "Users", icon: IconUser },
-  { link: "/settings", label: "Settings", icon: IconSettings },
+  { link: '/challenges', label: 'Challenges', icon: IconTrophy },
+  { link: '/users', label: 'Users', icon: IconUser },
+  { link: '/settings', label: 'Settings', icon: IconSettings }
 ];
 
 export function Navbar() {
-  const [active, setActive] = useState(0);
-
-  const links = data.map((item, index) => (
-    <NavLink
-      key={item.label}
-      active={index === active}
-      label={item.label}
-      href={item.link}
-      leftSection={<item.icon stroke={1.5} />}
-      onClick={() => setActive(index)}
-      variant="filled"
-    />
-  ));
+  const [activeNavIndex, setActiveNavIndex] = useState(0);
 
   return (
     <Flex direction="column" justify="space-between" h="100%">
       <Flex direction="column">
-        <Group
-          justify="space-between"
-          pb="var(--mantine-spacing-md)"
-          mb="calc(var(--mantine-spacing-md) * 1.5)"
-        >
+        <Group justify="space-between" pb="md" mb="calc(var(--mantine-spacing-md) * 1.5)">
           <Title order={3}>EngLead</Title>
         </Group>
-        {links}
+
+        <Group gap="0">
+          {data.map((item, index) => {
+            const { icon: Icon, label, link } = item;
+
+            return (
+              <NavLink
+                key={label}
+                href={link}
+                label={label}
+                variant="filled"
+                component={Link}
+                active={index === activeNavIndex}
+                leftSection={<Icon stroke={1.5} />}
+                onClick={() => setActiveNavIndex(index)}
+              />
+            );
+          })}
+        </Group>
       </Flex>
 
-      <Box pb="var(--mantine-spacing-md)" mb="var(--mantine-spacing-md)">
-        <NavLink
-          label="Change account"
-          href="/change-account"
-          leftSection={<IconSwitchHorizontal stroke={1.5} />}
-        />
-
-        <NavLink
-          label="Logout"
-          href="/logout"
-          leftSection={<IconLogout stroke={1.5} />}
-        />
-      </Box>
+      <Group gap="0" mb="md">
+        <NavLink label="Change Account" href="/change-account" leftSection={<IconSwitchHorizontal stroke={1.5} />} />
+        <NavLink label="Logout" href="/logout" leftSection={<IconLogout stroke={1.5} />} />
+      </Group>
     </Flex>
   );
 }
