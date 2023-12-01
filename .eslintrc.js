46339be00ac21b0f56b092ce946b0a1b176f5294
Alina Dangol <alinagoal>
2023-12-01 12:49:10 +0545

    config: husky and lint configurations and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,23 @@
-// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: [
+    "@repo/eslint-config/library.js",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  root: true,
 };
