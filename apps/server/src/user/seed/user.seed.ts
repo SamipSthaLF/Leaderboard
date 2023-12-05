ecb389c977c94398dd0d953fc43308f27e8993e2
asushrestha <shrestha.asmin17@gmail.com>
2023-12-05 16:29:06 +0545

    Fix: Code doc added for seed

--- apps/server/src/user/seed/user.seed.ts
+++ apps/server/src/user/seed/user.seed.ts
@@ -3,19 +3,37 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from '@/user/user.service';
 import { CreateUserDto } from '@/user/dto/create-user.dto';
 
+/**
+ * Service for seeding initial users in the database.
+ *
+ * @class
+ */
 @Injectable()
 export class UserSeed {
+  /**
+   * Constructor for the `UserSeed` service.
+   *
+   * @constructor
+   * @param {UserService} userService - The user service.
+   */
   constructor(private readonly userService: UserService) {}
 
-  async seed() {
-    // Check if user already exist in the database
-    const exisitingUsers = await this.userService.findAll();
-    if (exisitingUsers.length === 0) {
+  /**
+   * Seed initial users in the database if no users exist.
+   *
+   * @async
+   * @function
+   * @returns {Promise<void>} - A promise that resolves when the seeding is complete.
+   */
+  async seed(): Promise<void> {
+    // Check if users already exist in the database
+    const existingUsers = await this.userService.findAll();
+
+    if (existingUsers.length === 0) {
       // Users do not exist, seed some initial data
-      const seedUser = this.userService.create(
+      this.userService.create(
         new CreateUserDto('asminshrestha@lftechnology.com'),
       );
-
       // Add more users as needed
     }
   }
