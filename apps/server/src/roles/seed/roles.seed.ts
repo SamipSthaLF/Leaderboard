6a3b57c99191b16a2d3eca6f0e8613471b9c22f0
asushrestha <shrestha.asmin17@gmail.com>
2023-12-05 16:29:06 +0545

    Fix: Code doc added for seed

--- apps/server/src/roles/seed/roles.seed.ts
+++ apps/server/src/roles/seed/roles.seed.ts
@@ -5,13 +5,32 @@ import { RoleEnum } from '@common/constants/role.enum';
 import { RolesService } from '@/roles/roles.service';
 import { CreateRoleDto } from '@/roles/dto/create-role.dto';
 
+/**
+ * Service for seeding initial roles in the database.
+ *
+ * @class
+ */
 @Injectable()
 export class RoleSeed {
+  /**
+   * Constructor for the `RoleSeed` service.
+   *
+   * @constructor
+   * @param {RolesService} roleService - The roles service.
+   */
   constructor(private readonly roleService: RolesService) {}
 
-  async seed() {
+  /**
+   * Seed initial roles in the database if no roles exist.
+   *
+   * @async
+   * @function
+   * @returns {Promise<void>} - A promise that resolves when the seeding is complete.
+   */
+  async seed(): Promise<void> {
     // Check if roles already exist in the database
     const existingRoles = await this.roleService.findAll();
+
     if (existingRoles.length === 0) {
       // Roles do not exist, seed some initial data
       await this.roleService.create(
@@ -23,6 +42,7 @@ export class RoleSeed {
       await this.roleService.create(
         new CreateRoleDto(RoleEnum.Reviewer.toString()),
       );
+
       // Add more roles as needed
     }
   }
--- apps/server/src/userroles/seed/userroles.seed.ts
+++ apps/server/src/userroles/seed/userroles.seed.ts
@@ -10,36 +10,60 @@ import { RolesService } from '@/roles/roles.service';
 
 import { RoleEnum } from '@common/constants/role.enum';
 
+/**
+ * Service for seeding initial user roles in the database.
+ *
+ * @class
+ */
 @Injectable()
 export class UserroleSeed {
+  /**
+   * Constructor for the `UserroleSeed` service.
+   *
+   * @constructor
+   * @param {UserService} userService - The user service.
+   * @param {RolesService} roleService - The roles service.
+   * @param {ConfigService} configService - The configuration service.
+   */
   constructor(
     private readonly userService: UserService,
     private readonly roleService: RolesService,
     private readonly configService: ConfigService,
   ) {}
 
-  async seed() {
+  /**
+   * Seed initial user roles in the database if no users exist.
+   *
+   * @async
+   * @function
+   * @returns {Promise<void>} - A promise that resolves when the seeding is complete.
+   */
+  async seed(): Promise<void> {
     // Check if user already exist in the database
-    const exisitingUsers = await this.userService.findAll();
-    if (exisitingUsers.length === 0) {
+    const existingUsers = await this.userService.findAll();
+
+    if (existingUsers.length === 0) {
       // Users do not exist, seed some initial data
       const username = getSeedUser(this.configService).username;
       console.log(username);
+
       const user = await this.userService.findByUserName(username);
+
       if (!user) {
         const newUser = new User();
         newUser.createdOn = new Date().toDateString();
         newUser.lastLoginTime = new Date().toDateString();
         newUser.username = username;
+
         const role = await this.roleService.findByRoleName(
           RoleEnum.Admin.toString(),
         );
+
         if (role) {
           newUser.roles = [role];
           this.userService.saveUser(newUser);
         }
       }
-
       // Add more users as needed
     }
   }
