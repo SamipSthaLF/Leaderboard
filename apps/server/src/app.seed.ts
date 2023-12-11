1fdc795295dd205186f096fddfd14fac00d33db1
asushrestha <shrestha.asmin17@gmail.com>
2023-12-11 13:35:14 +0545

    Fix: EL-33: Configure application to use singleton instance - Update seed logic to take input param `INestApplication`

--- apps/server/src/app.seed.ts
+++ apps/server/src/app.seed.ts
@@ -1,10 +1,8 @@
-import { createApp } from '@/app.factory';
+import { INestApplication } from '@nestjs/common';
 
 import { UserSeed } from '@/user/seed/user.seed';
 
-async function seed() {
-  const app = await createApp();
-
+async function seed(app: INestApplication) {
   /**Seed data
    * Seeds the user to <SEED_USER_EMAIL]> if not present and assign default role to the user
    */
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,12 +3,15 @@ import { Reflector } from '@nestjs/core';
 import { createApp } from '@/app.factory';
 
 import { JwtAuthGuard } from '@filter/jwt-auth.guard';
+import { seed } from '@/app.seed';
 
 async function bootstrap() {
   const app = await createApp();
 
   app.enableCors();
   app.useGlobalGuards(new JwtAuthGuard(new Reflector()));
+
+  await seed(app);
 
   await app.listen(3001);
 }
